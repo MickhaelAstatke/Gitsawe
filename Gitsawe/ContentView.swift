50deddeb2d6e24e6d6e393fd34dd242baeff341b
Fekadesilassie Ayenew <fekadesilassie@aratayna.com>
2023-12-27 22:19:47 -0600

    Initial Commit

---
+++ Gitsawe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gitsawe
+//
+//  Created by Fekadesilassie on 12/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gitsawe/GitsaweApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitsaweApp.swift
+//  Gitsawe
+//
+//  Created by Fekadesilassie on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct GitsaweApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
